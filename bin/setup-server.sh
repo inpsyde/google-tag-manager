489a44d18ba36bbd9b1fbd833aea88b71b80b8d0
ChriCo <family.brueckner@googlemail.com>
2017-10-24 13:07:33 +0200

    fixed changed slug in setup-server.sh

--- bin/setup-server.sh
+++ bin/setup-server.sh
@@ -8,7 +8,7 @@ set -ex
 php -S "$WORDPRESS_URL" -t "$WORDPRESS_DIR" >/dev/null 2>&1 &
 
 # symlink the plugin in the WordPress plugin folder
-ln -s $TRAVIS_BUILD_DIR $WORDPRESS_DIR/wp-content/plugins/google-tag-manager
+ln -s $TRAVIS_BUILD_DIR $WORDPRESS_DIR/wp-content/plugins/inpsyde-google-tag-manager
 
 # show the plugins folder contents to make sure the plugin folder is there
 ls $WORDPRESS_DIR/wp-content/plugins
