f2d7f7664a431a16b8e4dd9e6f99c75d5f36f871
ChriCo <family.brueckner@googlemail.com>
2017-11-09 10:15:41 +0100

    removed generated assets from version control

--- gulpfile.js
+++ gulpfile.js
@@ -6,53 +6,50 @@ const uglify = require( 'gulp-uglify' );
 const rename = require( 'gulp-rename' );
 const sass = require( 'gulp-sass' );
 const cssnano = require( 'gulp-cssnano' );
+const imagemin = require( 'gulp-imagemin' );
+const svgmin = require( 'gulp-svgmin' );
 const mq = require( 'gulp-combine-mq' );
 const autoprefixer = require( 'gulp-autoprefixer' );
 const selenium = require( 'selenium-standalone' );
+const zip = require( 'gulp-zip' );
+const childProcess = require( 'child_process' );
+const util = require( 'gulp-util' );
+const fs = require( 'fs' );
+const CONFIG = JSON.parse( fs.readFileSync( './package.json' ) ).config;
 
-const ASSET_DIR = 'assets/';
-const CONF = {
-	js : {
-		src : ASSET_DIR + 'js/src/',
-		dest: ASSET_DIR + 'js/dist/'
-	},
-	css: {
-		src : ASSET_DIR + 'scss/',
-		dest: ASSET_DIR + 'css/'
+const createExecCallback = ( cb ) => ( err, stdout, stderr ) => {
+	if ( stdout ) {
+		util.log( stdout );
 	}
+	if ( stderr ) {
+		util.log( stderr );
+	}
+	cb( err );
 };
 
 gulp.task( 'scripts', function() {
-	const dest = CONF.js.dest;
+	const dest = CONFIG.scripts.dest;
 
-	gulp.src( [ CONF.js.src + '/*.js' ] )
+	gulp.src( [ CONFIG.scripts.src + '/*.js' ] )
 		.pipe( gulp.dest( dest ) )
-		.pipe( rename( {
-			extname: '.min.js'
-		} ) )
-		.pipe( uglify( {
-			output: {
-				ascii_only: true
-			}
-		} ) )
+		.pipe( rename( { extname: '.min.js' } ) )
+		.pipe( uglify( { output: { ascii_only: true } } ) )
 		.pipe( gulp.dest( dest ) );
 } );
 
 gulp.task( 'styles', function() {
-	gulp.src( CONF.css.src + '*.scss' )
+	gulp.src( CONFIG.styles.src + '*.scss' )
 		.pipe( sass( {
 			indentType : 'tab',
 			indentWidth: 1,
 			outputStyle: 'expanded'
 		} ) )
 		.pipe( mq() )
-		.pipe( autoprefixer( {
-			cascade: false
-		} ) )
-		.pipe( gulp.dest( CONF.css.dest ) )
+		.pipe( autoprefixer( { cascade: false } ) )
+		.pipe( gulp.dest( CONFIG.styles.dest ) )
 		.pipe( cssnano( { 'zindex': false } ) )
 		.pipe( rename( { suffix: '.min' } ) )
-		.pipe( gulp.dest( CONF.css.dest ) )
+		.pipe( gulp.dest( CONFIG.styles.dest ) );
 } );
 
 // @link http://silvenon.com/selenium-testing-workflow-with-webdriverio/
@@ -78,5 +75,51 @@ gulp.task( 'selenium', done => {
 		} );
 } );
 
-// Main task
-gulp.task( 'default', [ 'styles', 'scripts' ] );
+gulp.task( 'svn-assets', () => {
+	const dest = CONFIG.svn_assets.dest;
+
+	return gulp.src( `${CONFIG.svn_assets.src}*.{gif,jpeg,jpg,png}` )
+		.pipe( imagemin( { optimizationLevel: 7 } ) )
+		.pipe( gulp.dest( dest ) );
+} );
+
+gulp.task( 'images', () => {
+	const dest = CONFIG.images.dest;
+
+	return gulp.src( `${CONFIG.images.src}*.{gif,jpeg,jpg,png}` ).pipe( imagemin( { optimizationLevel: 7 } ) ).pipe( gulp.dest( dest ) )
+		&& gulp.src( `${CONFIG.images.src}*.svg` ).pipe( svgmin() ).pipe( gulp.dest( dest ) );
+} );
+
+gulp.task( 'composer-production', ( cb ) => {
+	// we don't want scripts and dev-dependencies in our release.
+	childProcess.exec( 'composer install --no-ansi --no-dev --no-interaction --no-progress --no-scripts --optimize-autoloader', createExecCallback( cb ) );
+} );
+gulp.task( 'composer-develop', ( cb ) => {
+	// we don't want scripts and dev-dependencies in our release.
+	childProcess.exec( 'composer install', createExecCallback( cb ) );
+} );
+
+gulp.task( 'develop', [ 'images', 'scripts', 'styles', 'composer-develop' ] );
+
+gulp.task( 'release', [ 'images', 'svn-assets', 'scripts', 'styles', 'composer-production' ], () => {
+
+	return gulp
+		.src( [
+			`${CONFIG.images.dest}**/*`,
+			`${CONFIG.scripts.dest}*.js`,
+			`${CONFIG.styles.dest}*.css`,
+			'src/**/*.php',
+			'vendor/**/*.php',
+			'config/*.php',
+			'*.{php,txt}',
+			'LICENSE',
+			'!report-*.txt',
+		], {
+			base: '.'
+		} )
+		.pipe( rename( ( path ) => {
+			path.dirname = `${CONFIG.slug}/${path.dirname}`;
+		} ) )
+		.pipe( zip( `${CONFIG.name}.zip` ) )
+		.pipe( gulp.dest( '.' ) );
+} );
