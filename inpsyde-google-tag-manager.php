8936367c5220d109cbf162f90055bc9d82be7297
Christian Leucht <3417446+Chrico@users.noreply.github.com>
2020-02-27 16:55:27 +0100

    version 1.3

--- inpsyde-google-tag-manager.php
+++ inpsyde-google-tag-manager.php
@@ -3,7 +3,7 @@
  * Plugin Name: Inpsyde Google Tag Manager
  * Description: Adds the Google Tag Manager container snippet to your site and populates the Google Tag Manager Data Layer.
  * Plugin URI:  https://wordpress.org/plugins/inpsyde-google-tag-manager
- * Version:     1.2
+ * Version:     1.3
  * Author:      Inpsyde GmbH 
  * Author URI:  https://inpsyde.com
  * Licence:     GPLv3
