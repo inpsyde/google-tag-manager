3c9b40471ebcf464e0755d1f5cb6fa77faa031c1
chrico <c.leucht@inpsyde.com>
2021-04-27 13:24:24 +0200

    NoscriptTagRenderer // adding tests.

--- tests/phpunit/Unit/Renderer/NoscriptTagRendererTest.php
+++ tests/phpunit/Unit/Renderer/NoscriptTagRendererTest.php
@@ -13,21 +13,18 @@ use Mockery;
 
 class NoscriptTagRendererTest extends AbstractTestCase
 {
-
     public function test_basic()
     {
-
         $testee = new NoscriptTagRenderer(Mockery::mock(DataLayer::class));
         static::assertInstanceOf(NoscriptTagRenderer::class, $testee);
     }
 
     public function test_render()
     {
-
-        $expected_id   = 'GTM-123456';
+        $expected_id = 'GTM-123456';
         $expected_data = ['foo' => 'bar'];
 
-        $first_url    = NoscriptTagRenderer::GTM_URL . '?id=' . $expected_id;
+        $first_url = NoscriptTagRenderer::GTM_URL . '?id=' . $expected_id;
         $expected_url = $first_url . '&foo=bar';
 
         $data = Mockery::mock(DataCollectorInterface::class);
@@ -67,7 +64,6 @@ class NoscriptTagRendererTest extends AbstractTestCase
 
     public function test_render__invalid_id()
     {
-
         Actions\expectDone(LogEvent::ACTION)
             ->once();
 
@@ -83,5 +79,56 @@ class NoscriptTagRendererTest extends AbstractTestCase
         $output = ob_get_clean();
 
         static::assertSame('', $output);
+    }
+
+    public function test_render_at_body_start__no_auto_insert()
+    {
+        static::expectOutputString('');
+
+        $dataLayer = Mockery::mock(DataLayer::class);
+        $dataLayer->shouldReceive('autoInsertNoscript')
+            ->once()
+            ->andReturn(false);
+
+        $testee = new NoscriptTagRenderer($dataLayer);
+        $testee->renderAtBodyStart([]);
+    }
+
+    public function test_render_at_body_start__no_noscript()
+    {
+        static::expectOutputString('');
+
+        $dataLayer = Mockery::mock(DataLayer::class);
+        $dataLayer->shouldReceive('autoInsertNoscript')
+            ->once()
+            ->andReturn(true);
+        $dataLayer->shouldReceive('id')
+            ->once()
+            ->andReturn('');
+
+        $testee = new NoscriptTagRenderer($dataLayer);
+        $testee->renderAtBodyStart([]);
+    }
+
+    public function test_render_at_body_start()
+    {
+        $dataLayer = Mockery::mock(DataLayer::class);
+        $dataLayer->shouldReceive('autoInsertNoscript')
+            ->once()
+            ->andReturn(true);
+        $dataLayer->shouldReceive('id')
+            ->once()
+            ->andReturn('');
+        $dataLayer->shouldReceive('data')
+            ->once()
+            ->andReturn([]);
+
+        Functions\stubs(['add_query_arg' => '']);
+
+        $testee = new NoscriptTagRenderer($dataLayer);
+        ob_start();
+        $testee->renderAtBodyStart([]);
+        $output = ob_get_clean();
+        static::assertContains("<iframe ", $output);
     }
 }
