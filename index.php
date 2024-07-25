b877a4705d230a546890c55ccde7fb588e25bdd3
Christian Leucht <c.leucht@inpsyde.com>
2024-07-25 10:06:59 +0200

    3.0.0-beta.1

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * Plugin Name: Inpsyde Google Tag Manager
  * Description: Adds the GTM container snippet to your site and populates the Data Layer.
  * Plugin URI:  https://wordpress.org/plugins/inpsyde-google-tag-manager
- * Version:     2.2.0
+ * Version:     3.0.0-beta.1
  * Author:      Syde GmbH
  * Author URI:  https://syde.com/
  * Text Domain: inpsyde-google-tag-manager
