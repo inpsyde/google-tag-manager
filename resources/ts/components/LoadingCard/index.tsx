d1d2b17a5ca2d7caee1f4fc48632034541f477c7
Christian Leucht <c.leucht@inpsyde.com>
2024-07-29 13:42:43 +0200

    LoadingCard // add leaf-beat.svg loading icon.

--- resources/ts/components/LoadingCard/index.tsx
+++ resources/ts/components/LoadingCard/index.tsx
@@ -1,13 +1,29 @@
 /**
  * WordPress dependencies
  */
-import { Card, CardBody, Spinner } from '@wordpress/components';
+import {
+	Card,
+	CardBody,
+	__experimentalHStack as HStack,
+	__experimentalText as Text,
+} from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import { ReactComponent as LeafBeat } from '../../../images/leaf-beat.svg';
 
 export const LoadingCard: FunctionComponent = () => {
 	return (
 		<Card>
 			<CardBody>
-				<Spinner />
+				<HStack justify="start" spacing="4">
+					<LeafBeat />
+					<Text size="large">
+						{ __( 'Loading…', 'inpsyde-google-tag-manager' ) }
+					</Text>
+				</HStack>
 			</CardBody>
 		</Card>
 	);
