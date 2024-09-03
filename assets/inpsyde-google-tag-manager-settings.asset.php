a7bb777df5d5db7162b8078899a7fdce156231b5
inpsyde-deploybot <github-deploybot@inpsyde.com>
2024-09-03 14:19:24 +0000

    [BOT] Add compiled assets for #refs/heads/master

--- assets/inpsyde-google-tag-manager-settings.asset.php
+++ assets/inpsyde-google-tag-manager-settings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e4481fa5d1d494d1a86d');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '670d034ee5f5eb5b70e5');
