d178a24e6214aa07f5467e9543ee0116d6e487f8
Christian Leucht <c.leucht@inpsyde.com>
2024-09-03 16:15:41 +0200

    renamed "DataLayerEndpoint" to "SettingsPageEndpoint".

--- resources/ts/api/get-settings-page.ts
+++ resources/ts/api/get-settings-page.ts
@@ -3,13 +3,10 @@
  */
 import apiFetch from '@wordpress/api-fetch';
 
-export const updateDataLayer = (
-	settings: Settings
-): Promise< DataLayerResponse > => {
+export const getSettingsPage = (): Promise< DataLayerResponse > => {
 	const namespace = InpsydeGoogleTagManager.Rest.namespace;
 	return apiFetch( {
-		path: namespace + '/data-layer',
-		method: 'POST',
-		data: settings,
+		path: namespace + '/settings-page',
+		method: 'GET',
 	} );
 };
