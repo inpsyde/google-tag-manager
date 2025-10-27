7ae4aa63d38d1c92176fb24278ef96011b8e487a
inpsyde-deploybot <github-deploybot@inpsyde.com>
2025-10-27 10:56:32 +0000

    [BOT] Add build artifact from dev/change-build-workflow -> change-build-workflow.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * Plugin Name: Inpsyde Google Tag Manager
  * Description: Adds the GTM container snippet to your site and populates the Data Layer.
  * Plugin URI:  https://wordpress.org/plugins/inpsyde-google-tag-manager
- * Version:     3.0.0
+ * Version: 3.0.0+change-build-workflow.967d324
  * Author:      Syde GmbH
  * Author URI:  https://syde.com/
  * Text Domain: inpsyde-google-tag-manager
