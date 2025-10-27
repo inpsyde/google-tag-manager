d34fcbcadd34d63008af3d5fcc312fc4321144a5
inpsyde-deploybot <github-deploybot@inpsyde.com>
2025-10-27 13:46:58 +0000

    [BOT] Add build artifact from dev/master -> master.

--- resources/ts/api/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Internal dependencies
- */
-import { getSettingsPage } from './get-settings-page';
-import { updateSettingsPage } from './update-settings-page';
-
-export { getSettingsPage, updateSettingsPage };
---
+++ vendor/composer/installed.php
@@ -0,0 +1,45 @@
+<?php return array(
+    'root' => array(
+        'name' => 'inpsyde/google-tag-manager',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '27b3d616a179d7b360c9ce1474a5506d91c10730',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'inpsyde/google-tag-manager' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '27b3d616a179d7b360c9ce1474a5506d91c10730',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'inpsyde/modularity' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'e1ca1c81b7b663355906b586525d21ac5d46bc65',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../inpsyde/modularity',
+            'aliases' => array(
+                0 => '1.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'psr/container' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '707984727bd5b2b670e59559d3ed2500240cf875',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/container',
+            'aliases' => array(
+                0 => '2.0.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
